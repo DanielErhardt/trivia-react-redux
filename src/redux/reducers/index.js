df5ccaf3015526687078faf1993af0c422423765
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-05-24 16:13:45 -0300

    criada página de Login, alguns teste já implementados

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import player from './player';
+// import wallet from './wallet';
+
+// const reducer = combineReducers({ user, wallet });
+const reducer = combineReducers({ player });
+
+export default reducer;
