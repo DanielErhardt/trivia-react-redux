1ae5f8fe123dc45587462d1f4f979dffd3aaa7d1
paulo_dragoni <paulohbfdf@gmail.com>
2022-05-26 17:20:48 -0300

    Requisito 18 - Botão da página Ranking redireciona para a tela inicial

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  clickToLogin =() => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <h2 data-testid="ranking-title">Ranking</h2>
+      <div>
+        <h2 data-testid="ranking-title">Ranking</h2>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.clickToLogin }
+        >
+          Login
+
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape.isRequired,
+};
 
 export default Ranking;
